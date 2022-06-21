423549d077a4adfc2fdfc727e4cb1534efc6dd14
Jesse Russo <jesserusso90@gmail.com>
2022-06-21 00:12:10 -0700

    solutions 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+let textArray = text.split('').reverse();
+let revWords = "";
+for(let i = 0; i < textArray.length; i++){
+    revWords += textArray[i];
+}
+return revWords;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
+    const nums = inputArray;
+    for(let i = 1; i < arguments.length; i++){
+        if(nums.indexOf(arguments[i]) > -1){
+            let newArray = nums.splice(nums.indexOf(arguments[i]), 1);
+        } 
+    }
 
-};
-
+    return nums;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
