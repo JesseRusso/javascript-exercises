a54401b1efd99f9ea003c7b29354dddb1f20819b
Jesse Russo <jesserusso90@gmail.com>
2022-06-22 23:59:43 -0700

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+let finalNum = 0;
+let min = Math.min(...arguments);
+let max = Math.max(...arguments);
 
+if(typeof arguments[0] === 'number' && typeof arguments[1] === 'number'){
+    if(min > 0 && max >0){
+        for(let i = 0; i <= max; i++){
+            finalNum += i;
+        }
+        return finalNum;
+    }
+}
+return "ERROR";
 };
 
 // Do not edit below this line
