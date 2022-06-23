133f4e180473356ecbc547cce3ea909ec24ca0e5
Jesse Russo <jesserusso90@gmail.com>
2022-06-23 00:30:30 -0700

    exercise 06

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+let newTemp = 0;
+newTemp = ((temp -32)/1.8).toFixed(1);
+return Number(newTemp);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp = 0;
+newTemp = (temp * 1.8 + 32).toFixed(1);
+return Number(newTemp);
 };
 
 // Do not edit below this line
